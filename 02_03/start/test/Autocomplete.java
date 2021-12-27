31186a80861bc1d6fd3a8a3ce729db1cef557232
mjpoulin <m.poulin1212@gmail.com>
2021-12-26 20:43:09 -0500

    Initial commit

---
+++ 02_03/start/test/Autocomplete.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/Matt/Downloads/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+        autocomplete.sendKeys("165 Barretts Hill Rd, Hudson, NH");
+        driver.quit();
+    }
+}
